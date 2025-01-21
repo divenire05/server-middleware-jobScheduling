9cfb7c252a88dc4a4dae11a09a0947b18891f6a6
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-21 13:24:37 -0500

    put and post requests

--- server.js
+++ server.js
@@ -2,12 +2,35 @@ const express = require('express')
 const app = express()
 const PORT = 4000
 
+
+// middleware
+app.use(express.json())
+
+
+// temp database
+const db = []
+
+
 app.get('/', (req, res) => {
     console.log('You have reached the home route: GET')
     res.status(200).send({"message": "Hi mom"})
 })
 
-app.delete('/', (req, res) => {
+app.post('/api/info', (req, res) => {
+    const {information} = req.body
+    console.log('THE POSTED MESSAGE WAS: ', information)
+    db.push(information)
+    console.log('DB: ', db)
+    res.status(201).json({'yourMessage': information})
+})
+
+app.put('/api', (req, res) => {
+    const {information} = req.body
+    console.log(information)
+    res.sendStatus(200)
+})
+
+app.delete('/delete/:id', (req, res) => {
     console.log('What do you want to delete?')
     res.sendStatus(200)
 })
