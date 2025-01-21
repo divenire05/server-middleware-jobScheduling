c85f74cb7d8ac630b9667e83c82df489fc8b5b24
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-21 15:42:13 -0500

    adding middleware for protection

--- server.js
+++ server.js
@@ -7,10 +7,24 @@ const PORT = 4000
 app.use(express.json())
 app.use(express.static('public'))
 app.use(require('cors')())
+app.use(mw)
+
+function mw(req, res, next) {
+    console.log('HIT THE MIDDLEWARE')
+    const {id} = req.query
+    console.log(id)
+    if (id != 8) {
+        return res.sendStatus(403)
+    }
+    next()
+}
 
 
 // temp database
 const db = []
+
+
+// scheduler
 
 
 // Cannot override express.static since it serves index.html to GET request from default home route
@@ -18,6 +32,11 @@ const db = []
 //     console.log('You have reached the home route: GET')
 //     res.sendStatus(200)
 // })
+
+app.delete('/', (req, res) => {
+    console.log('You have reached the home route: Delete')
+    res.sendStatus(200)
+})
 
 app.post('/api/info', (req, res) => {
     const {information} = req.body
@@ -38,9 +57,9 @@ app.delete('/delete/divenire/cool', (req, res) => {
     res.send('Didnt make it')
 })
 
-app.delete('/delete/:id/:name', (req, res) => {
-    const {id, name} = req.params 
-    console.log('What do you want to delete?', id, name)
+app.delete('/delete', (req, res) => {
+    const {id} = req.params 
+    console.log('What do you want to delete?', id)
     res.sendStatus(200)
 })
 
